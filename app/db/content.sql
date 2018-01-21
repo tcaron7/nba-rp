f37dd3f81360a01878493ba46b52d33c546cfdd3
MissChai <clemyfaure@gmail.com>
2018-01-21 15:19:37 +0100

    refactor sql files

--- app/db/content.sql
+++ app/db/content.sql
@@ -1,73 +1,11 @@
--- phpMyAdmin SQL Dump
--- version 4.1.14
--- http://www.phpmyadmin.net
---
--- Client :  127.0.0.1
--- Généré le :  Ven 22 Décembre 2017 à 18:33
--- Version du serveur :  5.6.17
--- Version de PHP :  5.5.12
-
-SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
-SET time_zone = "+00:00";
-
-
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8 */;
-
---
--- Base de données :  `nba`
---
-
--- --------------------------------------------------------
-
---
--- Structure de la table `awards`
---
-
-CREATE TABLE IF NOT EXISTS `awards` (
-  `awardId` int(11) NOT NULL AUTO_INCREMENT,
-  `season` int(11) NOT NULL,
-  `month` int(11) NOT NULL,
-  `award` varchar(200) NOT NULL,
-  `playerId` int(11) NOT NULL,
-  `teamId` int(11) NOT NULL,
-  PRIMARY KEY (`awardId`),
-  UNIQUE KEY `awardId` (`awardId`)
-) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=5 ;
-
---
 -- Contenu de la table `awards`
---
-
 INSERT INTO `awards` (`awardId`, `season`, `month`, `award`, `playerId`, `teamId`) VALUES
 (1, 2054, 11, 'Eastern Rookie of The Month', 405, 10),
 (2, 2054, 11, 'Western Rookie of The Month', 404, 29),
 (3, 2054, 11, 'Western Player of The Month', 281, 24),
 (4, 2054, 11, 'Eastern Player of The Month', 187, 13);
 
--- --------------------------------------------------------
-
---
--- Structure de la table `draftpick`
---
-
-CREATE TABLE IF NOT EXISTS `draftpick` (
-  `draftPickId` int(11) NOT NULL AUTO_INCREMENT,
-  `year` int(11) NOT NULL,
-  `round` int(11) NOT NULL,
-  `choiceNumber` int(11) NOT NULL,
-  `playerId` int(11) NOT NULL,
-  `originalOwnerTeamId` int(11) NOT NULL,
-  `currentOwnerTeamId` int(11) NOT NULL,
-  PRIMARY KEY (`draftPickId`)
-) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=1638 ;
-
---
 -- Contenu de la table `draftpick`
---
-
 INSERT INTO `draftpick` (`draftPickId`, `year`, `round`, `choiceNumber`, `playerId`, `originalOwnerTeamId`, `currentOwnerTeamId`) VALUES
 (2, 2053, 1, 1, 423, 10, 10),
 (3, 2053, 2, 3, 421, 22, 12),
@@ -862,31 +800,7 @@ INSERT INTO `draftpick` (`draftPickId`, `year`, `round`, `choiceNumber`, `player
 (1635, 2043, 2, 30, 917, 0, 0),
 (1637, 2043, 1, 12, 878, 0, 0);
 
--- --------------------------------------------------------
-
---
--- Structure de la table `game`
---
-
-CREATE TABLE IF NOT EXISTS `game` (
-  `gameId` int(11) NOT NULL AUTO_INCREMENT,
-  `season` int(11) NOT NULL,
-  `date` date NOT NULL,
-  `homeTeamId` int(11) NOT NULL,
-  `visitorTeamId` int(11) NOT NULL,
-  `homeTeamScore` int(11) NOT NULL,
-  `visitorTeamScore` int(11) NOT NULL,
-  `overtime` int(11) NOT NULL,
-  `winnerId` int(11) NOT NULL,
-  `loserId` int(11) NOT NULL,
-  `status` tinyint(1) NOT NULL,
-  PRIMARY KEY (`gameId`)
-) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=1231 ;
-
---
 -- Contenu de la table `game`
---
-
 INSERT INTO `game` (`gameId`, `season`, `date`, `homeTeamId`, `visitorTeamId`, `homeTeamScore`, `visitorTeamScore`, `overtime`, `winnerId`, `loserId`, `status`) VALUES
 (1, 2054, '2054-04-12', 29, 13, 0, 0, 0, 0, 0, 0),
 (2, 2054, '2054-04-12', 8, 20, 0, 0, 0, 0, 0, 0),
@@ -2120,25 +2034,7 @@ INSERT INTO `game` (`gameId`, `season`, `date`, `homeTeamId`, `visitorTeamId`, `
 (1229, 2054, '2054-03-15', 29, 14, 0, 0, 0, 0, 0, 0),
 (1230, 2054, '2053-11-03', 19, 8, 94, 102, 0, 8, 19, 1);
 
--- --------------------------------------------------------
-
---
--- Structure de la table `injury`
---
-
-CREATE TABLE IF NOT EXISTS `injury` (
-  `injuryId` int(11) NOT NULL AUTO_INCREMENT,
-  `playerId` int(11) NOT NULL,
-  `injuryDate` date NOT NULL,
-  `recoveryDate` date NOT NULL,
-  `injurySeverity` varchar(25) NOT NULL,
-  PRIMARY KEY (`injuryId`)
-) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=52 ;
-
---
 -- Contenu de la table `injury`
---
-
 INSERT INTO `injury` (`injuryId`, `playerId`, `injuryDate`, `recoveryDate`, `injurySeverity`) VALUES
 (1, 134, '2053-10-27', '2053-11-07', 'Unavailable'),
 (2, 264, '2053-10-27', '2053-12-01', 'Unavailable'),
@@ -2192,45 +2088,11 @@ INSERT INTO `injury` (`injuryId`, `playerId`, `injuryDate`, `recoveryDate`, `inj
 (50, 195, '2053-12-15', '2053-12-20', 'Unavailable'),
 (51, 160, '2053-12-16', '2054-01-30', 'Unavailable');
 
--- --------------------------------------------------------
-
---
--- Structure de la table `nbacurrentdate`
---
-
-CREATE TABLE IF NOT EXISTS `nbacurrentdate` (
-  `nbaDate` date NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
-
---
 -- Contenu de la table `nbacurrentdate`
---
-
 INSERT INTO `nbacurrentdate` (`nbaDate`) VALUES
 ('2053-12-17');
 
--- --------------------------------------------------------
-
---
--- Structure de la table `person`
---
-
-CREATE TABLE IF NOT EXISTS `person` (
-  `personId` int(11) NOT NULL AUTO_INCREMENT,
-  `firstname` varchar(25) NOT NULL,
-  `name` varchar(25) NOT NULL,
-  `birthdate` date NOT NULL,
-  `nationality` varchar(25) NOT NULL,
-  `formation` varchar(25) NOT NULL,
-  `height` float NOT NULL,
-  `weight` int(11) NOT NULL,
-  PRIMARY KEY (`personId`)
-) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=918 ;
-
---
 -- Contenu de la table `person`
---
-
 INSERT INTO `person` (`personId`, `firstname`, `name`, `birthdate`, `nationality`, `formation`, `height`, `weight`) VALUES
 (1, 'Robert', 'Paxtor', '2016-05-13', 'US', 'Wake Forest', 2.16, 131),
 (2, 'Patrick', 'Sutterly', '2016-08-12', 'US', 'Duke', 1.98, 94),
@@ -3129,31 +2991,7 @@ INSERT INTO `person` (`personId`, `firstname`, `name`, `birthdate`, `nationality
 (916, 'Theo', 'Miron', '2022-01-18', 'Belgian', 'ASVEL', 1.98, 95),
 (917, 'Gary', 'Kurwell', '2022-11-17', 'English', 'London Lions', 2.07, 101);
 
--- --------------------------------------------------------
-
---
--- Structure de la table `player`
---
-
-CREATE TABLE IF NOT EXISTS `player` (
-  `playerId` int(11) NOT NULL AUTO_INCREMENT,
-  `personId` int(11) NOT NULL,
-  `teamId` int(11) NOT NULL,
-  `position` varchar(2) NOT NULL,
-  `salary` float NOT NULL,
-  `guarantedYear` int(11) NOT NULL,
-  `optionalYear` int(11) NOT NULL,
-  `contractType` varchar(6) NOT NULL,
-  `experience` int(11) NOT NULL,
-  `draftPromotion` int(11) NOT NULL,
-  `draftPosition` int(11) NOT NULL,
-  PRIMARY KEY (`playerId`)
-) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=809 ;
-
---
 -- Contenu de la table `player`
---
-
 INSERT INTO `player` (`playerId`, `personId`, `teamId`, `position`, `salary`, `guarantedYear`, `optionalYear`, `contractType`, `experience`, `draftPromotion`, `draftPosition`) VALUES
 (1, 1, 31, 'C', 0, 0, 0, '', 19, 2034, 2),
 (2, 2, 5, 'SG', 9.8, 1, 1, 'Player', 19, 2034, 1),
@@ -3926,27 +3764,7 @@ INSERT INTO `player` (`playerId`, `personId`, `teamId`, `position`, `salary`, `g
 (807, 916, 0, 'SG', 0, 0, 0, '', 1, 2043, 59),
 (808, 917, 0, 'SF', 0, 0, 0, '', 2, 2043, 60);
 
--- --------------------------------------------------------
-
---
--- Structure de la table `prospect`
---
-
-CREATE TABLE IF NOT EXISTS `prospect` (
-  `prospectId` int(11) NOT NULL AUTO_INCREMENT,
-  `personId` int(11) NOT NULL,
-  `position` varchar(2) NOT NULL,
-  `ranking` int(11) NOT NULL,
-  `predictedDraftYear` int(11) NOT NULL,
-  `actualDraftYear` int(11) NOT NULL,
-  `cursusType` varchar(25) NOT NULL,
-  PRIMARY KEY (`prospectId`)
-) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=216 ;
-
---
 -- Contenu de la table `prospect`
---
-
 INSERT INTO `prospect` (`prospectId`, `personId`, `position`, `ranking`, `predictedDraftYear`, `actualDraftYear`, `cursusType`) VALUES
 (1, 387, 'SG', 2, 2053, 2053, 'NCAA'),
 (2, 388, 'C', 1, 2053, 2053, 'NCAA'),
@@ -4164,26 +3982,7 @@ INSERT INTO `prospect` (`prospectId`, `personId`, `position`, `ranking`, `predic
 (214, 631, 'PF', 49, 2057, 2057, 'NCAA'),
 (215, 632, 'C', 50, 2057, 2057, 'NCAA');
 
--- --------------------------------------------------------
-
---
--- Structure de la table `rookiecontract`
---
-
-CREATE TABLE IF NOT EXISTS `rookiecontract` (
-  `pick` int(11) NOT NULL AUTO_INCREMENT,
-  `salary` float NOT NULL,
-  `guarantedYear` int(11) NOT NULL,
-  `optionalYear` int(11) NOT NULL,
-  `contractType` varchar(25) NOT NULL,
-  PRIMARY KEY (`pick`),
-  UNIQUE KEY `pick` (`pick`)
-) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=61 ;
-
---
 -- Contenu de la table `rookiecontract`
---
-
 INSERT INTO `rookiecontract` (`pick`, `salary`, `guarantedYear`, `optionalYear`, `contractType`) VALUES
 (1, 4.8, 2, 2, 'Rookie'),
 (2, 4.5, 2, 2, 'Rookie'),
@@ -4246,35 +4045,7 @@ INSERT INTO `rookiecontract` (`pick`, `salary`, `guarantedYear`, `optionalYear`,
 (59, 0.8, 1, 3, 'Rookie'),
 (60, 0.8, 1, 3, 'Rookie');
 
--- --------------------------------------------------------
-
---
--- Structure de la table `season`
---
-
-CREATE TABLE IF NOT EXISTS `season` (
-  `year` int(11) NOT NULL AUTO_INCREMENT,
-  `champion` int(11) NOT NULL,
-  `finalist` int(11) NOT NULL,
-  `status` int(11) NOT NULL,
-  `startDate` date NOT NULL,
-  `stopDate` date NOT NULL,
-  `tradeLimitDate` date NOT NULL,
-  `signatureLimitDate` date NOT NULL,
-  `restrictedFreeAgentOptionDate` date NOT NULL,
-  `allStarDate` date NOT NULL,
-  `regularSeasonAwardsDate` date NOT NULL,
-  `draftDate` date NOT NULL,
-  `maxPlayersInTeam` int(11) NOT NULL,
-  `salaryCap` int(11) NOT NULL,
-  `contractMax` int(11) NOT NULL,
-  PRIMARY KEY (`year`)
-) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=2055 ;
-
---
 -- Contenu de la table `season`
---
-
 INSERT INTO `season` (`year`, `champion`, `finalist`, `status`, `startDate`, `stopDate`, `tradeLimitDate`, `signatureLimitDate`, `restrictedFreeAgentOptionDate`, `allStarDate`, `regularSeasonAwardsDate`, `draftDate`, `maxPlayersInTeam`, `salaryCap`, `contractMax`) VALUES
 (2035, 0, 0, 0, '0000-00-00', '0000-00-00', '0000-00-00', '0000-00-00', '0000-00-00', '0000-00-00', '0000-00-00', '0000-00-00', 0, 0, 0),
 (2036, 0, 0, 0, '0000-00-00', '0000-00-00', '0000-00-00', '0000-00-00', '0000-00-00', '0000-00-00', '0000-00-00', '0000-00-00', 0, 0, 0),
@@ -4297,28 +4068,7 @@ INSERT INTO `season` (`year`, `champion`, `finalist`, `status`, `startDate`, `st
 (2053, 3, 11, 0, '2052-07-01', '2053-06-30', '0000-00-00', '0000-00-00', '0000-00-00', '0000-00-00', '0000-00-00', '2053-06-28', 0, 0, 0),
 (2054, 0, 0, 1, '2053-07-01', '2054-06-30', '2054-02-22', '2054-03-15', '2053-08-01', '2054-02-14', '2054-04-23', '2054-06-28', 15, 90, 30);
 
--- --------------------------------------------------------
-
---
--- Structure de la table `signature`
---
-
-CREATE TABLE IF NOT EXISTS `signature` (
-  `signatureId` int(11) NOT NULL AUTO_INCREMENT,
-  `transactionId` int(11) NOT NULL,
-  `teamId` int(11) NOT NULL,
-  `personId` int(11) NOT NULL,
-  `salary` double NOT NULL,
-  `guarantedYear` int(11) NOT NULL,
-  `optionalYear` int(11) NOT NULL,
-  `contractType` varchar(25) NOT NULL,
-  PRIMARY KEY (`signatureId`)
-) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=139 ;
-
---
 -- Contenu de la table `signature`
---
-
 INSERT INTO `signature` (`signatureId`, `transactionId`, `teamId`, `personId`, `salary`, `guarantedYear`, `optionalYear`, `contractType`) VALUES
 (4, 11, 24, 321, 18.9, 4, 1, 'Player'),
 (5, 12, 6, 122, 21.2, 3, 1, 'Player'),
@@ -4455,41 +4205,7 @@ INSERT INTO `signature` (`signatureId`, `transactionId`, `teamId`, `personId`, `
 (137, 158, 5, 710, 1, 1, 0, ''),
 (138, 159, 22, 653, 1, 1, 0, '');
 
--- --------------------------------------------------------
-
---
--- Structure de la table `statplayer`
---
-
-CREATE TABLE IF NOT EXISTS `statplayer` (
-  `statsId` int(11) NOT NULL AUTO_INCREMENT,
-  `playerId` int(11) NOT NULL,
-  `season` int(11) NOT NULL,
-  `teamId` int(11) NOT NULL,
-  `games` int(11) NOT NULL,
-  `minutes` int(11) NOT NULL,
-  `points` int(11) NOT NULL,
-  `freeThrowsMade` int(11) NOT NULL,
-  `freeThrowsAttempt` int(11) NOT NULL,
-  `twoPointsMade` int(11) NOT NULL,
-  `twoPointsAttempt` int(11) NOT NULL,
-  `threePointsMade` int(11) NOT NULL,
-  `threePointsAttempt` int(11) NOT NULL,
-  `offensiveRebounds` int(11) NOT NULL,
-  `defensiveRebounds` int(11) NOT NULL,
-  `rebounds` int(11) NOT NULL,
-  `assists` int(11) NOT NULL,
-  `turnovers` int(11) NOT NULL,
-  `steals` int(11) NOT NULL,
-  `blocks` int(11) NOT NULL,
-  `evaluation` int(11) NOT NULL,
-  PRIMARY KEY (`statsId`)
-) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=1841 ;
-
---
 -- Contenu de la table `statplayer`
---
-
 INSERT INTO `statplayer` (`statsId`, `playerId`, `season`, `teamId`, `games`, `minutes`, `points`, `freeThrowsMade`, `freeThrowsAttempt`, `twoPointsMade`, `twoPointsAttempt`, `threePointsMade`, `threePointsAttempt`, `offensiveRebounds`, `defensiveRebounds`, `rebounds`, `assists`, `turnovers`, `steals`, `blocks`, `evaluation`) VALUES
 (1, 281, 2051, 24, 79, 2463, 1598, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0),
 (2, 281, 2052, 24, 76, 2512, 1922, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0),
@@ -6331,41 +6047,7 @@ INSERT INTO `statplayer` (`statsId`, `playerId`, `season`, `teamId`, `games`, `m
 (1839, 601, 2050, 29, 53, 1447, 673, 0, 0, 0, 0, 0, 0, 32, 103, 135, 77, 75, 28, 15, 0),
 (1840, 601, 2051, 7, 45, 723, 385, 0, 0, 0, 0, 0, 0, 18, 79, 97, 56, 49, 12, 8, 0);
 
--- --------------------------------------------------------
-
---
--- Structure de la table `statsgame`
---
-
-CREATE TABLE IF NOT EXISTS `statsgame` (
-  `statsId` int(11) NOT NULL AUTO_INCREMENT,
-  `playerId` int(11) NOT NULL,
-  `playerTeamId` int(11) NOT NULL,
-  `season` int(11) NOT NULL,
-  `gameId` int(11) NOT NULL,
-  `minutes` int(11) NOT NULL,
-  `points` int(11) NOT NULL,
-  `freeThrowsMade` int(11) NOT NULL,
-  `freeThrowsAttempt` int(11) NOT NULL,
-  `twoPointsMade` int(11) NOT NULL,
-  `twoPointsAttempt` int(11) NOT NULL,
-  `threePointsMade` int(11) NOT NULL,
-  `threePointsAttempt` int(11) NOT NULL,
-  `offensiveRebounds` int(11) NOT NULL,
-  `defensiveRebounds` int(11) NOT NULL,
-  `rebounds` int(11) NOT NULL,
-  `assists` int(11) NOT NULL,
-  `turnovers` int(11) NOT NULL,
-  `steals` int(11) NOT NULL,
-  `blocks` int(11) NOT NULL,
-  `evaluation` int(11) NOT NULL,
-  PRIMARY KEY (`statsId`)
-) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=8789 ;
-
---
 -- Contenu de la table `statsgame`
---
-
 INSERT INTO `statsgame` (`statsId`, `playerId`, `playerTeamId`, `season`, `gameId`, `minutes`, `points`, `freeThrowsMade`, `freeThrowsAttempt`, `twoPointsMade`, `twoPointsAttempt`, `threePointsMade`, `threePointsAttempt`, `offensiveRebounds`, `defensiveRebounds`, `rebounds`, `assists`, `turnovers`, `steals`, `blocks`, `evaluation`) VALUES
 (1, 184, 13, 2054, 3, 27, 13, 1, 2, 6, 10, 0, 0, 2, 8, 10, 1, 2, 1, 3, 21),
 (2, 421, 13, 2054, 3, 7, 2, 0, 0, 1, 3, 0, 0, 0, 6, 6, 0, 1, 0, 1, 6),
@@ -15091,28 +14773,7 @@ INSERT INTO `statsgame` (`statsId`, `playerId`, `playerTeamId`, `season`, `gameI
 (8787, 75, 1, 2054, 1127, 17, 7, 0, 0, 2, 5, 1, 1, 0, 2, 2, 1, 1, 0, 0, 6),
 (8788, 0, 1, 2054, 1127, 240, 104, 18, 23, 31, 60, 8, 21, 9, 34, 43, 21, 17, 9, 5, 118);
 
--- --------------------------------------------------------
-
---
--- Structure de la table `team`
---
-
-CREATE TABLE IF NOT EXISTS `team` (
-  `teamId` int(11) NOT NULL AUTO_INCREMENT,
-  `city` varchar(25) NOT NULL,
-  `name` varchar(25) NOT NULL,
-  `abbreviation` varchar(3) NOT NULL,
-  `conference` varchar(4) NOT NULL,
-  `division` varchar(25) NOT NULL,
-  `mainColor` varchar(255) NOT NULL,
-  `secondaryColor` varchar(255) NOT NULL,
-  PRIMARY KEY (`teamId`)
-) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=31 ;
-
---
 -- Contenu de la table `team`
---
-
 INSERT INTO `team` (`teamId`, `city`, `name`, `abbreviation`, `conference`, `division`, `mainColor`, `secondaryColor`) VALUES
 (1, 'Philadelphia', '76ers', 'PHI', 'East', 'Atlantic', '#0000A0', ''),
 (2, 'Milwaukee', 'Bucks', 'MIL', 'East', 'Central', '#EDE275', ''),
@@ -15145,26 +14806,7 @@ INSERT INTO `team` (`teamId`, `city`, `name`, `abbreviation`, `conference`, `div
 (29, 'Golden State', 'Warriors', 'GS', 'West', 'Pacific', '#FEC547', ''),
 (30, 'Washington', 'Wizards', 'WAS', 'East', 'Southeast', '#368BC1', '');
 
--- --------------------------------------------------------
-
---
--- Structure de la table `tradeelement`
---
-
-CREATE TABLE IF NOT EXISTS `tradeelement` (
-  `tradeElementId` int(11) NOT NULL AUTO_INCREMENT,
-  `transactionId` int(11) NOT NULL,
-  `giverTeamId` int(11) NOT NULL,
-  `receiverTeamId` int(11) NOT NULL,
-  `tradeElement` int(11) NOT NULL,
-  `tradeElementType` varchar(25) NOT NULL,
-  PRIMARY KEY (`tradeElementId`)
-) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=76 ;
-
---
 -- Contenu de la table `tradeelement`
---
-
 INSERT INTO `tradeelement` (`tradeElementId`, `transactionId`, `giverTeamId`, `receiverTeamId`, `tradeElement`, `tradeElementType`) VALUES
 (1, 1, 13, 25, 186, 'player'),
 (2, 1, 25, 13, 328, 'player'),
@@ -15242,22 +14884,7 @@ INSERT INTO `tradeelement` (`tradeElementId`, `transactionId`, `giverTeamId`, `r
 (74, 156, 27, 6, 460, 'player'),
 (75, 156, 27, 6, 346, 'player');
 
--- --------------------------------------------------------
-
---
--- Structure de la table `transaction`
---
-
-CREATE TABLE IF NOT EXISTS `transaction` (
-  `transactionId` int(11) NOT NULL AUTO_INCREMENT,
-  `date` date NOT NULL,
-  PRIMARY KEY (`transactionId`)
-) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=160 ;
-
---
 -- Contenu de la table `transaction`
---
-
 INSERT INTO `transaction` (`transactionId`, `date`) VALUES
 (1, '2053-06-28'),
 (2, '2053-06-28'),
@@ -15417,7 +15044,3 @@ INSERT INTO `transaction` (`transactionId`, `date`) VALUES
 (157, '2053-12-16'),
 (158, '2053-12-16'),
 (159, '2053-12-16');
-
-/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
-/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
-/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
