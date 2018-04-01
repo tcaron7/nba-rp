2fb1ff738f49117af57852b5a6ef89d330aff03a
MissChai <clemyfaure@gmail.com>
2018-04-01 21:18:57 +0200

    remove titles

--- index.php
+++ index.php
@@ -38,14 +38,14 @@ require_once( 'app/routes.php' );
 
 ?>
 
-<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
-<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="fr" >
+<!DOCTYPE html>
+<html>
 	<head>
 		<meta charset="iso-8859-1" />
-		
+
 		<!-- CSS -->
 		<link href="<?php echo $GLOBALS['path']['css']; ?>default.css" rel="stylesheet" type="text/css" />
-		
+
 		<!-- JS -->
 		<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
 		<script src="<?php echo $GLOBALS['path']['js']; ?>navDropdown.js"></script>
@@ -55,111 +55,13 @@ require_once( 'app/routes.php' );
 
 		<!-- FAVICON -->
 		<link rel="icon" type="image/png" href="<?php echo $GLOBALS['path']['img']; ?>favicon.png" />
-		
+
 		<!-- TITLE -->
-		<?php
-		$title = '';
-		
-		if (!isset($_GET['section']) OR $_GET['section'] == 'index' OR $_GET['section'] == 'next_day')
-		{
-			$title = 'Accueil NBA';
-		}
-		else if ($_GET['section'] == 'play')
-		{
-				$title = 'Fill NBA Games';
-		}
-		else if ($_GET['section'] == 'season_view' and isset($_GET['year']))
-		{
-			$title = 'NBA Saison';
-		}
-		else if ($_GET['section'] == 'team_view' and isset($_GET['id']))
-		{
-			$title = 'Team';
-		}
-		else if ($_GET['section'] == 'schedule')
-		{
-			$title = 'NBA Schedule';
-		}
-		else if ($_GET['section'] == 'player')
-		{
-			$title = 'NBA Players';
-		}
-		else if ($_GET['section'] == 'add_player')
-		{
-			$title = 'Add Players';
-		}
-		else if ($_GET['section'] == 'standing')
-		{
-			$title = 'NBA Standings';
-		}
-		else if ($_GET['section'] == 'stats')
-		{
-			$title = 'NBA Stats';
-		}
-		else if ($_GET['section'] == 'player_season_stats')
-		{
-			$title = 'NBA Player Stats';
-		}
-		else if ($_GET['section'] == 'prospects')
-		{
-			$title = 'NBA Prospects Ranking';
-		}
-		else if ($_GET['section'] == 'add_prospect')
-		{
-			$title = 'Add Players';
-		}
-			else if ($_GET['section'] == 'trade')
-		{
-			$title = 'Trade';
-		}
-			else if ($_GET['section'] == 'signature')
-		{
-			$title = 'Signature';
-		}
-			else if ($_GET['section'] == 'sign_player' or $_GET['section'] == 'sign_rookie')
-		{
-			$title = 'Sign Player';
-		}
-			else if ($_GET['section'] == 'lottery')
-		{
-			$title = 'Draft Lottery';
-		}
-			else if ($_GET['section'] == 'draft_subscription')
-		{
-			$title = 'Draft Prospect Subscription';
-		}
-			else if ($_GET['section'] == 'draft' || $_GET['section'] == 'select_prospect')
-		{
-			$title = 'NBA Draft';
-		}
-			else if ($_GET['section'] == 'season_transition')
-		{
-			$title = 'NBA Season Transition';
-		}
-			else if ($_GET['section'] == 'players_option')
-		{
-			$title = 'NBA Activate option';
-		}
-			else if ($_GET['section'] == 'restricted_players_option')
-		{
-			$title = 'NBA Activate restricted player option';
-		}
-			else if ( ($_GET['section'] == 'injuryDisplay') || ($_GET['section'] == 'transactionDisplay') )
-		{
-			$title = 'NBA News';
-		}
-		else
-		{
-			$title = 'Unknow Page';
-		}
-		
-		echo "<title>" . $title . "</title>";
-		?>
+		<title>Play NBA!</title>
 	</head>
 
 	<body>
 		<div id="wrap">
-		
 			<?php
 
 			// Current Date
@@ -169,7 +71,7 @@ require_once( 'app/routes.php' );
 			echo '<header id="pageHeader">';
 				include_once( $GLOBALS['path']['views'] . 'header.php' );
 			echo '</header>';
-			
+
 			// Content
 			echo '<div id="pageContent">';
 				try
@@ -189,6 +91,5 @@ require_once( 'app/routes.php' );
 
 			?>
 		</div>
-
 	</body>
 </html>
